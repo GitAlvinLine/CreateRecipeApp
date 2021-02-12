8c08280e89d98774fb38bac211ae2e9ac102aa32
Alvin Escobar <alvincsproductions@gmail.com>
2021-02-11 18:19:54 -0600

    Initial Commit

---
+++ FoodManChu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodManChu
+//
+//  Created by Alvin Escobar on 2/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
